f7a75e5e0314e05a838591771733867b6697f495
li2xdd <494112274@qq.com>
2018-12-19 20:00:20 +0800

    提交一下下

---
+++ FirstTest/src/main/java/club/li2xdd/test/FirstMain.java
@@ -0,0 +1,10 @@
+package club.li2xdd.test;
+
+/**
+ * Created by li2xdd on 2018/12/19/0019.
+ */
+public class FirstMain {
+    public static void main(String[] args) {
+        System.out.println("你好！ 全世界！");
+    }
+}
---
+++ FirstTest/src/main/java/woqu.java
@@ -0,0 +1,8 @@
+/**
+ * Created by li2xdd on 2018/12/19/0019.
+ */
+public class woqu {
+    public static void main(String[] args) {
+        System.out.println("为什么不能数字开头呢？");
+    }
+}
